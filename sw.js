be14a80eccec73a03116114a95d5303d7ea9b09b
taterthxt <christian.tate1042@gmail.com>
2026-09-20 17:56:25 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='my-health-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-180.png','./hero_plant.png','./hero_plant2.png','./bottom_left_clean.png','./bottom_right_clean.png'];
+const CACHE='my-health-v2';
+const ASSETS=['./','./index.html','./manifest.json','./icon-180.png','./botanical-background.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return x}))));
